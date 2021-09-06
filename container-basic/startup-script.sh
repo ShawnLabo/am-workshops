3acbac0603d7e8d8933b07bb56d7a1c07a4b0ae8
nownabe <1286807+nownabe@users.noreply.github.com>
2021-09-06 12:06:59 +0900

    Create startup-script.sh

---
+++ container-basic/startup-script.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+mkdir -p /tmp/wordpress-app
+cat << EOF > /tmp/wordpress-app/docker-compose.yaml
+version: '3'
+services:
+   mysql:
+     image: mysql:5.7.9
+     volumes:
+       - mysql-data:/var/lib/mysql
+     restart: always
+     environment:
+       MYSQL_ROOT_PASSWORD: himitsupass
+       MYSQL_DATABASE: wordpress
+       MYSQL_USER: wordpress
+       MYSQL_PASSWORD: wordpress
+
+   wordpress:
+     depends_on:
+       - mysql
+     image: wordpress:latest
+     ports:
+       - "8000:80"
+     restart: always
+     environment:
+       WORDPRESS_DB_HOST: mysql:3306
+       WORDPRESS_DB_USER: wordpress
+       WORDPRESS_DB_PASSWORD: wordpress
+volumes:
+    mysql-data:
+EOF
